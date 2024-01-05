917b7162ac8c4b2352e69af3a2d4b1d1c1df928d
KODJO TAMEGNON ELISEE HENOC <helitako16@gmail.com>
2024-01-05 21:57:30 +0000

    aperçu 5

--- src/pages/[username].tsx
+++ src/pages/[username].tsx
@@ -61,7 +61,7 @@ const SendMessage = () => {
           content="Envoyer des messages anonymes"
         />
         <meta property="og:image" content="URL_de_votre_image_d'aperçu" />
-        <meta property="og:url" content="../assets/logo_bg_gradient.jpg" />
+        <meta property="og:url" content="/logo_bg_gradient.jpg" />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       {/* container principal */}
