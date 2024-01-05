1d1ba57053168f0ad95d08fbef06a194d61d0db3
KODJO TAMEGNON ELISEE HENOC <helitako16@gmail.com>
2024-01-05 22:04:40 +0000

    7

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,17 @@ config.autoAddCss = false;
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        {/* Ajoutez les balises meta d'aperçu ici */}
+        <meta property="og:title" content="Mystery Mailbox" />
+        <meta
+          property="og:description"
+          content="Envoyer des messages anonymes"
+        />
+        <meta property="og:image" content="URL_de_votre_image_d'aperçu" />
+        <meta property="og:url" content="logo_bg_gradient.jpg" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
       <ChakraProvider>
         <Component {...pageProps} />
       </ChakraProvider>
