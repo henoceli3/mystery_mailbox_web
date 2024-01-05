61998d373ac3866c269145914237e27212b1f9b1
KODJO TAMEGNON ELISEE HENOC <helitako16@gmail.com>
2024-01-05 21:12:10 +0000

    preview

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,26 @@ import { ChakraProvider, theme } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import Head from "next/head";
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        {/* Ajoutez les balises meta d'aperçu ici */}
+        <meta property="og:title" content="Mystery Mailbox" />
+        <meta
+          property="og:description"
+          content="Envoyer des messages anonymes"
+        />
+        <meta property="og:image" content="URL_de_votre_image_d'aperçu" />
+        <meta property="og:url" content="../assets/logo_bg_gradient.jpg" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
