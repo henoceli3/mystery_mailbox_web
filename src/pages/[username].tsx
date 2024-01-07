399e822323e7949511c4a38f593839133e13f3ec
KODJO TAMEGNON ELISEE HENOC <helitako16@gmail.com>
2024-01-07 19:37:14 +0000

    12

--- src/pages/[username].tsx
+++ src/pages/[username].tsx
@@ -52,6 +52,23 @@ const SendMessage = () => {
 
   return (
     <>
+      <Head>
+        {/* Ajoutez les balises meta d'aperçu ici */}
+        <title>Mystery Mailbox</title>
+        <meta name="description" content="Envoyer des messages anonymes" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="Mystery Mailbox" />
+        <meta
+          property="og:description"
+          content="Envoyer des messages anonymes"
+        />
+        <meta property="og:image" content="/logo_bg_gradient.png" />
+        <meta
+          property="og:url"
+          content="https://astonishing-cactus-f1ef9b.netlify.app/"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
       {/* container principal */}
       <Flex h="100vh" w="100%" flexDirection={"column"} alignItems={"center"}>
         {/* partie du haut  */}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,26 @@
 import { Center, Heading } from "@chakra-ui/react";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        {/* Ajoutez les balises meta d'aperçu ici */}
+        <title>Mystery Mailbox</title>
+        <meta name="description" content="Envoyer des messages anonymes" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="Mystery Mailbox" />
+        <meta
+          property="og:description"
+          content="Envoyer des messages anonymes"
+        />
+        <meta property="og:image" content="/logo_bg_gradient.png" />
+        <meta
+          property="og:url"
+          content="https://astonishing-cactus-f1ef9b.netlify.app/"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
       <Center h="100vh" w="100vw">
         <Heading>Home</Heading>
       </Center>
